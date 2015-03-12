23dd42115efd932093af8b6cd1da5bfde1645c65
Andrew Gwynn <Andrew@SeventhGamers.com>
2015-03-12 19:20:41 +0900

    Differentiated IDs from other Mods
    Noticed this while playing, that some topics had incorrect audience
    matches in the hints, and I can only guess this is because I'd played
    with other mods that had different settings. Also added a new topic and
    ticked the version # up.

--- Topic-Mod/7gtopics.js
+++ Topic-Mod/7gtopics.js
@@ -1,505 +1,525 @@
 (function () {
 		GDT.addTopics([
 		{
-			id: "Police",
+			id: "7G-Police",
 			name: "Police".localize("game topic"),
 			genreWeightings: [1, .7, .6, 1, .9, .8],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Assassin",
+			id: "7G-Assassin",
 			name: "Assassin".localize("game topic"),
 			genreWeightings: [1, .8, .8, .9, .6, .6],
 			audienceWeightings: [0.8, 0.9, 1]
 		} , {
-			id: "Steampunk",
+			id: "7G-Steampunk",
 			name: "Steampunk".localize("game topic"),
 			genreWeightings: [1, 1, 1, 1, 1, .9],
 			audienceWeightings: [0.9, 0.9, 1]
 		} , {
-			id: "Restaurant",
+			id: "7G-Restaurant",
 			name: "Restaurant".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, .9],
 			audienceWeightings: [0.7, 1, .8]
 		} , {
-			id: "Supervillain",
+			id: "7G-Supervillain",
 			name: "Supervillain".localize("game topic"),
 			genreWeightings: [.9, .6, 1, .7, .6, .7],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Survival",
+			id: "7G-Survival",
 			name: "Survival".localize("game topic"),
 			genreWeightings: [.7, .9, .8, 1, .6, .6],
 			audienceWeightings: [0.6, 1, 1]
 		} , {
-			id: "Matching",
+			id: "7G-Matching",
 			name: "Matching".localize("game topic"),
 			genreWeightings: [.6, .6, .6, .6, .6, 1],
 			audienceWeightings: [1, 0.8, .6]
 		} , {
-			id: "Childhood",
+			id: "7G-Childhood",
 			name: "Childhood".localize("game topic"),
 			genreWeightings: [.9, 1, .9, .7, .6, .6],
 			audienceWeightings: [0.9, 0.9, .6]
 		} , {
-			id: "Adulthood",
+			id: "7G-Adulthood",
 			name: "Adulthood".localize("game topic"),
 			genreWeightings: [.9, 1, .9, .7, .6, .6],
 			audienceWeightings: [.9, 0.9, 1]
 		} , {
-			id: "Robots",
+			id: "7G-Robots",
 			name: "Robots".localize("game topic"),
 			genreWeightings: [1, .7, .9, .6, 1, .7],
 			audienceWeightings: [0.9, 1, .9]
 		} , {
-			id: "Teen Angst",
+			id: "7G-Teen Angst",
 			name: "Teem Angst".localize("game topic"),
 			genreWeightings: [.8, 1, 1, .6, .6, .6],
 			audienceWeightings: [0.6, 1, .6]
 		} , {
-			id: "Underwater",
+			id: "7G-Underwater",
 			name: "Underwater".localize("game topic"),
 			genreWeightings: [.9, 1, .7, 1, 1, .8],
 			audienceWeightings: [0.9, 1, .9]
 		} , {
-			id: "Colonial Era",
+			id: "7G-Colonial Era",
 			name: "Colonial Era".localize("game topic"),
 			genreWeightings: [.9, 1, .7, .8, 1, .8],
 			audienceWeightings: [0.8, 1, .9]
 		} , {
-			id: "Automotive",
+			id: "7G-Automotive",
 			name: "Automotive".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .7, .9],
 			audienceWeightings: [0.6, 1, .9]
 		} , {
-			id: "Railroad",
+			id: "7G-Railroad",
 			name: "Railroad".localize("game topic"),
 			genreWeightings: [.6, .8, .6, 1, .9, .6],
 			audienceWeightings: [0.8, 1, .9]
 		} , {
-			id: "Ships",
+			id: "7G-Ships",
 			name: "Ships".localize("game topic"),
 			genreWeightings: [.9, .6, .6, .6, .6, .6],
 			audienceWeightings: [0.9, 1, .9]
 		} , {
-			id: "Renaissance",
+			id: "7G-Renaissance",
 			name: "Renaissance".localize("game topic"),
 			genreWeightings: [.9, 1, 1, .9, .9, .7],
 			audienceWeightings: [.7, 1, 1]
 		} , {
-			id: "Dark Ages",
+			id: "7G-Dark Ages",
 			name: "Dark Ages".localize("game topic"),
 			genreWeightings: [1, 1, 1, .6, .9, .6],
 			audienceWeightings: [0.9, 1, .9]
 		} , {
-			id: "Pre-History",
+			id: "7G-Pre-History",
 			name: "Pre-History".localize("game topic"),
 			genreWeightings: [1, 1, .8, .6, .8, .7],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Planet",
+			id: "7G-Planet",
 			name: "Planet".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .9, .9],
 			audienceWeightings: [0.9, 1, 1]
 		} , {
-			id: "Physics",
+			id: "7G-Physics",
 			name: "Physics".localize("game topic"),
 			genreWeightings: [.6, .9, .6, 1, .6, .9],
 			audienceWeightings: [0.8, 1, 1]
 		} , {
-			id: "Sword Fights",
+			id: "7G-Sword Fights",
 			name: "Sword Fights".localize("game topic"),
 			genreWeightings: [1, .6, .8, .9, .6, .9],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Building",
+			id: "7G-Building",
 			name: "Building".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .6, .9],
 			audienceWeightings: [0.9, 1, 1]
 		} , {
-			id: "World War 1",
+			id: "7G-World War 1",
 			name: "World War 1".localize("game topic"),
 			genreWeightings: [1, .6, .7, 1, 1, .6],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "World War 2",
+			id: "7G-World War 2",
 			name: "World War 2".localize("game topic"),
 			genreWeightings: [1, .6, .8, 1, 1, .6],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Forensics",
+			id: "7G-Forensics",
 			name: "Forensics".localize("game topic"),
 			genreWeightings: [.6, .9, .6, 1, .6, .8],
 			audienceWeightings: [0.8, .9, 1]
 		} , {
-			id: "Universe",
+			id: "7G-Universe",
 			name: "Universe".localize("game topic"),
 			genreWeightings: [.6, 1, .8, 1, .9, .7],
 			audienceWeightings: [0.8, 1, .8]
 		} , {
-			id: "Future War",
+			id: "7G-Future War",
 			name: "Future War".localize("game topic"),
 			genreWeightings: [1, .6, .9, .9, 1, .8],
 			audienceWeightings: [0.9, 1, 1]
 		} , {
-			id: "Modern",
+			id: "7G-Modern",
 			name: "Modern".localize("game topic"),
 			genreWeightings: [1, .9, 1, .7, 1, .6],
 			audienceWeightings: [0.6, 1, 1]
 		} , {
-			id: "Rockets",
+			id: "7G-Rockets",
 			name: "Rockets".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .9, .9],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Demolition",
+			id: "7G-Demolition",
 			name: "Demolition".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, 1, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Superpowers",
+			id: "7G-Superpowers",
 			name: "Superpowers".localize("game topic"),
 			genreWeightings: [1, .6, .9, .6, .6, .8],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Dragons",
+			id: "7G-Dragons",
 			name: "Dragons".localize("game topic"),
-			genreWeightings: [1, .9, .9, .6, .8, .8],
+			genreWeightings: [.7, 1, .8, .9, .8, .7],
 			audienceWeightings: [1, 1, .9]
 		} , {
-			id: "Crime",
+			id: "7G-Crime",
 			name: "Crime".localize("game topic"),
 			genreWeightings: [1, .6, .8, .9, .9, .9],
 			audienceWeightings: [.9, 1, 1]
 		} , {
-			id: "Animals",
+			id: "7G-Animals",
 			name: "Animals".localize("game topic"),
 			genreWeightings: [.6, .9, .7, 1, .8, 1],
 			audienceWeightings: [1, 0.9, .8]
 		} , {
-			id: "Horses",
+			id: "7G-Horses",
 			name: "Horses".localize("game topic"),
 			genreWeightings: [.6, .9, .7, 1, .8, 1],
 			audienceWeightings: [1, 0.9, .8]
 		} , {
-			id: "Dogs",
+			id: "7G-Dogs",
 			name: "Dogs".localize("game topic"),
 			genreWeightings: [.6, .9, .7, 1, .8, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Cats",
+			id: "7G-Cats",
 			name: "Cats".localize("game topic"),
 			genreWeightings: [.6, .9, .7, 1, .8, 1],
 			audienceWeightings: [1, 0.9, 1]
 		} , {
-			id: "Fish",
+			id: "7G-Fish",
 			name: "Fish".localize("game topic"),
 			genreWeightings: [.6, .9, .7, 1, .8, 1],
 			audienceWeightings: [0.9, 0.9, .9]
 		} , {
-			id: "Zoos",
+			id: "7G-Zoos",
 			name: "Zoos".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, .9],
 			audienceWeightings: [0.9, 1, .8]
 		} , {
-			id: "Dinosaurs",
+			id: "7G-Dinosaurs",
 			name: "Dinosaurs".localize("game topic"),
 			genreWeightings: [1, 1, .6, .9, .9, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Gene Splicing",
+			id: "7G-Gene Splicing",
 			name: "Gene Splicing".localize("game topic"),
 			genreWeightings: [.6, .6, .8, .9, 1, 1],
 			audienceWeightings: [.7, .8, 1]
 		} , {
-			id: "Solar System",
+			id: "7G-Solar System",
 			name: "Solar System".localize("game topic"),
 			genreWeightings: [.6, 1, .6, 1, .9, .9],
 			audienceWeightings: [0.9, 1, 1]
 		} , {
-			id: "Disease",
+			id: "7G-Disease",
 			name: "Disease".localize("game topic"),
 			genreWeightings: [.6, .8, .6, 1, .6, 1],
 			audienceWeightings: [.8, 1, .8]
 		} , {
-			id: "Shopkeeping",
+			id: "7G-Shopkeeping",
 			name: "Shopkeeping".localize("game topic"),
 			genreWeightings: [.6, 1, 1, 1, 1, 1],
 			audienceWeightings: [0.7, 1, 1]
 		} , {
-			id: "Family",
+			id: "7G-Family",
 			name: "Family".localize("game topic"),
 			genreWeightings: [.6, 1, 1, .8, .6, .8],
 			audienceWeightings: [1, .8, .8]
 		} , {
-			id: "Castles",
+			id: "7G-Castles",
 			name: "Castles".localize("game topic"),
 			genreWeightings: [.6, .8, .9, 1, .9, .8],
 			audienceWeightings: [.7, 1, .9]
 		} , {
-			id: "Mechs",
+			id: "7G-Mechs",
 			name: "Mechs".localize("game topic"),
 			genreWeightings: [1, .6, .8, 1, 1, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Goats",
+			id: "7G-Goats",
 			name: "Goats".localize("game topic"),
 			genreWeightings: [.6, .8, .8, 1, .6, .9],
 			audienceWeightings: [.9, 1, .9]
 		} , {
-			id: "Rocks",
+			id: "7G-Rocks",
 			name: "Rocks".localize("game topic"),
 			genreWeightings: [.6, .6, .6, .6, .6, .6],
 			audienceWeightings: [0.6, 0.6, 0.6]
 		} , {
-			id: "Octopuses",
+			id: "7G-Octopuses",
 			name: "Octopuses".localize("game topic"),
 			genreWeightings: [.6, .8, .8, 1, .8, .9],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Mice",
+			id: "7G-Mice",
 			name: "Mice".localize("game topic"),
 			genreWeightings: [.6, 1, .8, .8, .6, .9],
 			audienceWeightings: [1, .8, .6]
 		} , {
-			id: "Hamsters",
+			id: "7G-Hamsters",
 			name: "Hamsters".localize("game topic"),
 			genreWeightings: [.6, 1, .8, .8, .6, .9],
 			audienceWeightings: [1, .8, .6]
 		} , {
-			id: "Snakes",
+			id: "7G-Snakes",
 			name: "Snakes".localize("game topic"),
 			genreWeightings: [.6, .8, .6, 1, .6, .7],
 			audienceWeightings: [0.7, 0.8, 1]
 		} , {
-			id: "Bugs",
+			id: "7G-Bugs",
 			name: "Bugs".localize("game topic"),
 			genreWeightings: [.6, 1, 1, 1, 1, 1],
 			audienceWeightings: [.9, 0.9, 1]
 		} , {
-			id: "Stealth",
+			id: "7G-Stealth",
 			name: "Stealth".localize("game topic"),
 			genreWeightings: [1, 1, .8, .8, 1, .6],
 			audienceWeightings: [0.8, 0.9, 1]
 		} , {
-			id: "Dystopian",
+			id: "7G-Dystopian",
 			name: "Dystopian".localize("game topic"),
 			genreWeightings: [.8, 1, 1, .6, .8, .6],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Archery",
+			id: "7G-Archery",
 			name: "Archery".localize("game topic"),
 			genreWeightings: [.9, .6, .6, 1, .6, .9],
 			audienceWeightings: [.9, 1, .8]
 		} , {
-			id: "Tanks",
+			id: "7G-Tanks",
 			name: "Tanks".localize("game topic"),
 			genreWeightings: [1, .6, .6, 1, 1, .9],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Fishing",
+			id: "7G-Fishing",
 			name: "Fishing".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .6, .9],
 			audienceWeightings: [0.6, 1, .9]
 		} , {
-			id: "Boating",
+			id: "7G-Boating",
 			name: "Boating".localize("game topic"),
 			genreWeightings: [.6, .9, .6, 1, .6, .9],
 			audienceWeightings: [0.6, 0.9, 1]
 		} , {
-			id: "Bridges",
+			id: "7G-Bridges",
 			name: "Bridges".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .7, 1],
 			audienceWeightings: [.8, 1, .7]
 		} , {
-			id: "Cycling",
+			id: "7G-Cycling",
 			name: "Cycling".localize("game topic"),
 			genreWeightings: [.6, .6, .8, 1, .8, .9],
 			audienceWeightings: [0.6, 1, .9]
 		} , {
-			id: "Motorcycles",
+			id: "7G-Motorcycles",
 			name: "Motorcycles".localize("game topic"),
 			genreWeightings: [1, .9, .8, .9, .6, .9],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Skating",
+			id: "7G-Skating",
 			name: "Skating".localize("game topic"),
 			genreWeightings: [.8, .6, .6, 1, .6, .9],
 			audienceWeightings: [1, 1, .7]
 		} , {
-			id: "Skateboarding",
+			id: "7G-Skateboarding",
 			name: "Skateboarding".localize("game topic"),
 			genreWeightings: [.8, .6, .6, 1, .6, .9],
 			audienceWeightings: [1, 1, .7]
 		} , {
-			id: "Ice Skating",
+			id: "7G-Ice Skating",
 			name: "Ice Skating".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .6, 1],
 			audienceWeightings: [.7, 1, .7]
 		} , {
-			id: "Travel",
+			id: "7G-Travel",
 			name: "Travel".localize("game topic"),
 			genreWeightings: [.6, 1, .6, .9, .6, .9],
 			audienceWeightings: [.9, 1, .9]
 		} , {
-			id: "Economics",
+			id: "7G-Economics",
 			name: "Economics".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .9, .9],
 			audienceWeightings: [0.6, .8, 1]
 		} , {
-			id: "Stocks",
+			id: "7G-Stocks",
 			name: "Stocks".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .6, .7],
 			audienceWeightings: [0.6, .8, 1]
 		} , {
-			id: "Oil Rigs",
+			id: "7G-Oil Rigs",
 			name: "Oil Rigs".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, .8],
 			audienceWeightings: [0.6, 1, .8]
 		} , {
-			id: "Mining",
+			id: "7G-Mining",
 			name: "Mining".localize("game topic"),
 			genreWeightings: [.8, .6, .6, 1, .8, 1],
 			audienceWeightings: [0.8, 1, .8]
 		} , {
-			id: "Board Games",
+			id: "7G-Board Games",
 			name: "Board Games".localize("game topic"),
 			genreWeightings: [.6, 1, 1, 1, 1, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Educational",
+			id: "7G-Educational",
 			name: "Educational".localize("game topic"),
 			genreWeightings: [.6, .6, .6, .9, .6, 1],
 			audienceWeightings: [1, 1, .7]
 		} , {
-			id: "Card Games",
+			id: "7G-Card Games",
 			name: "Card Games".localize("game topic"),
 			genreWeightings: [.6, 1, .9, 1, 1, 1],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Baseball",
+			id: "7G-Baseball",
 			name: "Baseball".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Basketball",
+			id: "7G-Basketball",
 			name: "Basketball".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Soccer",
+			id: "7G-Soccer",
 			name: "Soccer".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Football",
+			id: "7G-Football",
 			name: "Football".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Hockey",
+			id: "7G-Hockey",
 			name: "Hockey".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Rugby",
+			id: "7G-Rugby",
 			name: "Rugby".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Parkour",
+			id: "7G-Parkour",
 			name: "Parkour".localize("game topic"),
 			genreWeightings: [1, .8, .6, 1, .6, 1],
 			audienceWeightings: [.9, 1, .9]
 		} , {
-			id: "Tennis",
+			id: "7G-Tennis",
 			name: "Tennis".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Volleyball",
+			id: "7G-Volleyball",
 			name: "Volleyball".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Beach Volleyball",
+			id: "7G-Beach Volleyball",
 			name: "Beach Volleyball".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, 1]
 		} , {
-			id: "Tower Defence",
+			id: "7G-Tower Defence",
 			name: "Tower Defence".localize("game topic"),
 			genreWeightings: [.9, .6, .8, .6, 1, 1],
 			audienceWeightings: [.9, 1, .9]
 		} , {
-			id: "Vikings",
+			id: "7G-Vikings",
 			name: "Vikings".localize("game topic"),
 			genreWeightings: [1, 1, 1, 1, 1, .8],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Skiing",
+			id: "7G-Skiing",
 			name: "Skiing".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Surfing",
+			id: "7G-Surfing",
 			name: "Surfing".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, 1],
 			audienceWeightings: [.7, 1, .8]
 		} , {
-			id: "Diving",
+			id: "7G-Diving",
 			name: "Diving".localize("game topic"),
 			genreWeightings: [.8, 1, .6, 1, .7, .9],
 			audienceWeightings: [.8, 1, .9]
 		} , {
-			id: "Politics",
+			id: "7G-Politics",
 			name: "Politics".localize("game topic"),
 			genreWeightings: [.6, .6, .8, 1, .7, .9],
 			audienceWeightings: [0.6, 0.8, 1]
 		} , {
-			id: "Chickens",
+			id: "7G-Chickens",
 			name: "Chickens".localize("game topic"),
 			genreWeightings: [1, .6, .7, .9, .6, .8],
 			audienceWeightings: [.8, 1, .8]
 		} , {
-			id: "Doctors",
+			id: "7G-Doctors",
 			name: "Doctors".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .6, .9],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Firefighting",
+			id: "7G-Firefighting",
 			name: "Firefighting".localize("game topic"),
 			genreWeightings: [.9, .6, .7, 1, .7, .8],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Astronauts",
+			id: "7G-Astronauts",
 			name: "Astronauts".localize("game topic"),
 			genreWeightings: [.8, 1, .8, 1, .7, .8],
 			audienceWeightings: [1, 1, 1]
 		} , {
-			id: "Asteroids",
+			id: "7G-Asteroids",
 			name: "Asteroids".localize("game topic"),
 			genreWeightings: [1, .6, .6, .8, .8, 1],
 			audienceWeightings: [.8, 1, .8]
 		} , {
-			id: "Farming",
+			id: "7G-Farming",
 			name: "Farming".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, .8],
 			audienceWeightings: [.7, 1, .7]
 		} , {
-			id: "Logging",
+			id: "7G-Logging",
 			name: "Logging".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .8, .8],
 			audienceWeightings: [.7, 1, .7]
 		} , {
-			id: "Banking",
+			id: "7G-Banking",
 			name: "Mining".localize("game topic"),
 			genreWeightings: [.6, .6, .6, 1, .7, .8],
 			audienceWeightings: [0.6, 1, .9]
 		} , {
-			id: "Toast",
+			id: "7G-Toast",
 			name: "Toast".localize("game topic"),
 			genreWeightings: [.6, .9, .6, 1, .6, 1],
 			audienceWeightings: [0.6, 0.9, 1]
+		} , {
+			id: "7G-Dwarf",
+			name: "Dwarf".localize("game topic"),
+			genreWeightings: [.6, .8, .8, 1, 1, .6],
+			audienceWeightings: [0.7, .9, 1]
+		} , {
+			id: "7G-Gnome",
+			name: "Gnome".localize("game topic"),
+			genreWeightings: [.6, .8, .7, .9, 1, .9],
+			audienceWeightings: [0.9, 1, .9]
+		} , {
+			id: "7G-Lions",
+			name: "Lions".localize("game topic"),
+			genreWeightings: [.7, 1, .8, 1, .7, .8],
+			audienceWeightings: [1, 1, 1]
+		} , {
+			id: "7G-Civilization",
+			name: "Civilization".localize("game topic"),
+			genreWeightings: [.7, 1, .8, 1, .7, .8],
+			audienceWeightings: [1, 1, 1]
 		}
 		]);
 	})();
